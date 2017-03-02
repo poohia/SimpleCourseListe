1283a368c9c5d637ea6ecdf238cfa2b28bae133b
jordan <jordan@TXW003>
2017-03-02 17:55:14 +0100

    add alerte box

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
 
 /******************* Pages *******************************/
 
@@ -13,15 +13,43 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'create.html'
 })
 export class CreatePage {
-  private headerTitle : string = "SimpleCourseLite";
+  private title : string = "";
 
-  constructor(public navCtrl: NavController) {
+  private prompt = this.alertCtrl.create({
+      title: 'Nom de vôtre liste',
+      message: "Entrez le nom de vôtre  liste :)",
+      inputs: [
+        {
+          name: '',
+          placeholder: '',
+          value : this.title
+        },
+      ],
+      buttons: [
+        {
+          text: 'Annuler',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Enregistrer',
+          handler: data => {
+            console.log('Saved clicked', data);
+          }
+        }
+      ]
+    });
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
       console.log("Create controller");
+      this.title = "Ma liste du 01/01/2016" ;
+      this.prompt.present();
   }
 
-  public getHeaderTitle() : string
+  public getTitle() : string
   {
-     return this.headerTitle;
+     return this.title;
   }
 
 
